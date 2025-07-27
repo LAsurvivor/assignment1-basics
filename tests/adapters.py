2ea5b26100c692fff996ded89f577f4f6bbf1695
Zhaohan Hu <lasurvivor1787@gmail.com>
2025-07-26 17:54:03 -0700

    Assignment 1: RMSNorm

--- tests/adapters.py
+++ tests/adapters.py
@@ -12,6 +12,7 @@ from torch import Tensor
 from cs336_basics.bpe_tokenizer import train_bpe, Tokenizer
 from cs336_basics.embedding import Embedding
 from cs336_basics.linear import Linear
+from cs336_basics.rmsnorm import RMSNorm
 
 
 def run_linear(
@@ -391,7 +392,13 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rmsnorm = RMSNorm(
+        d_model=d_model,
+        eps=eps,
+    )
+
+    rmsnorm.load_state_dict({"g": weights})
+    return rmsnorm.forward(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
