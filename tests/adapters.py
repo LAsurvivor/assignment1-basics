9239f051b44343d34d415f45ac8f82da3962febe
Zhaohan Hu <lasurvivor1787@gmail.com>
2025-08-03 14:29:04 -0700

    Assignment 1: checkpointing

--- tests/adapters.py
+++ tests/adapters.py
@@ -11,6 +11,7 @@ from torch import Tensor
 
 from cs336_basics.adamw import AdamW
 from cs336_basics.bpe_tokenizer import train_bpe, Tokenizer
+from cs336_basics.checkpointing import load_checkpoint, save_checkpoint
 from cs336_basics.cross_entropy import cross_entropy
 from cs336_basics.data_loading import get_batch
 from cs336_basics.embedding import Embedding
@@ -511,7 +512,8 @@ def run_get_batch(
         x=dataset,
         batch_size=batch_size,
         context_length=context_length,
-        device=device,)
+        device=device,
+    )
 
 
 def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, " ..."]:
@@ -615,7 +617,12 @@ def run_save_checkpoint(
             we've completed.
         out (str | os.PathLike | BinaryIO | IO[bytes]): Path or file-like object to serialize the model, optimizer, and iteration to.
     """
-    raise NotImplementedError
+    save_checkpoint(
+        model=model,
+        optimizer=optimizer,
+        iteration=iteration,
+        out=out,
+    )
 
 
 def run_load_checkpoint(
@@ -636,7 +643,11 @@ def run_load_checkpoint(
     Returns:
         int: the previously-serialized number of iterations.
     """
-    raise NotImplementedError
+    return load_checkpoint(
+        src=src,
+        model=model,
+        optimizer=optimizer,
+    )
 
 
 def get_tokenizer(
